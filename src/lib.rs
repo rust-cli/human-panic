642c6bfdf1658e1cfcd6cd90b8e61373374323c2
Katharina Sabel <katharina.sabel@asquera.de>
2018-04-16 18:19:32 +0200

    Changing the core functionality of the crate
    Instead of having to wrap around every panic, this now uses `set_hook` once
    to register the callback to print a pretty status message. This also has the
    added benefit of pulling in env! calls because the main setup was made
    into a macro.
    
    Optionally the two core functions (print_help and dump_log) can now also be used
    without the core macro, because they are `pub fn`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #![cfg_attr(feature = "nightly", deny(missing_docs))]
 #![cfg_attr(feature = "nightly", feature(external_doc))]
 #![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
@@ -5,36 +6,36 @@
 extern crate termcolor;
 
 use std::error::Error;
-use std::panic;
-
-/// Catch any error handlers that occur, and
-// Cargo env vars available:
-// https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-crates
-pub fn catch_unwind<F: FnOnce() -> Result<(), Box<Error>>>(f: F) {
-  panic::set_hook(Box::new(|_panic_info| {
-    // TODO: create log report.
-    if let Err(e) = print_msg() {
-      eprintln!("Error generating panic message: {}", e);
-    }
-  }));
-
-  if let Err(_e) = f() {
-    // TODO: create log report.
-  }
-}
-
+use std::panic::PanicInfo;
 use std::io::{Result as IoResult, Write};
 use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
 
-fn print_msg() -> IoResult<()> {
+
+/// Setup the human panic hook that will make all panics
+/// as beautiful as your shitty code.
+#[macro_export]
+macro_rules! setup_panic {
+  () => {
+    use std::env;
+    use std::panic::{self, PanicInfo};
+    use human_panic::*;
+    let _version = env!("CARGO_PKG_VERSION");
+    let name = env!("CARGO_PKG_NAME");
+    let authors = env!("CARGO_PKG_AUTHORS");
+    let homepage = env!("CARGO_PKG_HOMEPAGE");
+
+    panic::set_hook(Box::new(move |info: &PanicInfo| {
+      print_msg(_version, name, authors, homepage).unwrap();
+      handle_dump(info);
+    }));
+  };
+}
+
+/// Utility function that prints a message to our human users
+pub fn print_msg(_version: &str, name: &str, authors: &str, homepage: &str) -> IoResult<()> {
   let stderr = BufferWriter::stderr(ColorChoice::Auto);
   let mut buffer = stderr.buffer();
   buffer.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
-
-  let _version = env!("CARGO_PKG_VERSION");
-  let name = env!("CARGO_PKG_NAME");
-  let authors = env!("CARGO_PKG_AUTHORS");
-  let homepage = env!("CARGO_PKG_HOMEPAGE");
 
   writeln!(&mut buffer, "Well, this is embarrasing.\n")?;
   writeln!(&mut buffer, "{} had a problem and crashed. To help us diagnose the problem you can send us a crash report.\n", name)?;
@@ -50,6 +51,9 @@ fn print_msg() -> IoResult<()> {
   writeln!(&mut buffer, "Thank you kindly!")?;
 
   stderr.print(&buffer).unwrap();
-
   Ok(())
 }
+
+/// Utility function which will handle dumping information to disk
+/// TODO: Implement
+pub fn handle_dump(_panic_info: &PanicInfo) {}
