7a2e9230751abd3a152d3240a8b4c75891ae8e41
Katharina Sabel <katharina.sabel@asquera.de>
2018-04-16 18:50:56 +0200

    Merging #4 by yoshuawuyts
    This commit adds the ability to generate reports based on an application
    crash. It hooks into the existing panic handler and also exposes
    the report generation feature via a `pub fn`

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,13 @@
 #![cfg_attr(feature = "nightly", feature(external_doc))]
 #![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
 
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate serde_derive;
 extern crate termcolor;
 
+mod report;
 use std::panic::PanicInfo;
 use std::io::{Result as IoResult, Write};
 use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
@@ -23,21 +28,21 @@ macro_rules! setup_panic {
     let homepage = env!("CARGO_PKG_HOMEPAGE");
 
     panic::set_hook(Box::new(move |info: &PanicInfo| {
-      print_msg(_version, name, authors, homepage).unwrap();
-      handle_dump(info);
+      let file_path = handle_dump(info);
+      print_msg(file_path, _version, name, authors, homepage).unwrap();
     }));
   };
 }
 
 /// Utility function that prints a message to our human users
-pub fn print_msg(_version: &str, name: &str, authors: &str, homepage: &str) -> IoResult<()> {
+pub fn print_msg(file_path: String, _version: &str, name: &str, authors: &str, homepage: &str) -> IoResult<()> {
   let stderr = BufferWriter::stderr(ColorChoice::Auto);
   let mut buffer = stderr.buffer();
   buffer.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
 
   writeln!(&mut buffer, "Well, this is embarrasing.\n")?;
   writeln!(&mut buffer, "{} had a problem and crashed. To help us diagnose the problem you can send us a crash report.\n", name)?;
-  writeln!(&mut buffer, "We have generated a report file at \"<reports not generated yet>\". Submit an issue or email with the subject of \"{} Crash Report\" and include the report as an attachment.\n", name)?;
+  writeln!(&mut buffer, "We have generated a report file at \"{}\". Submit an issue or email with the subject of \"{} Crash Report\" and include the report as an attachment.\n", &file_path, name)?;
 
   if !homepage.is_empty() {
     writeln!(&mut buffer, "- Homepage: {}", homepage)?;
@@ -54,4 +59,7 @@ pub fn print_msg(_version: &str, name: &str, authors: &str, homepage: &str) -> I
 
 /// Utility function which will handle dumping information to disk
 /// TODO: Implement
-pub fn handle_dump(_panic_info: &PanicInfo) {}
+pub fn handle_dump(_panic_info: &PanicInfo) -> String {
+  let r = report::Report::new(report::Method::Err);
+  return r.persist().unwrap();
+}
