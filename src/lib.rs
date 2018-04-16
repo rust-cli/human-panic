b90ea3ba1cda64f65928f36429da67523e78dcfb
Pascal Hertleif <killercup@gmail.com>
2018-04-16 20:52:29 +0200

    Stable and slim (#1)
    * Make it compile on stable
    
    Also adds a nightly feature flag that will automatically be picked up by
    docs.rs to build nice docs.
    
    * Make clippy a CI-only dependency
    
    You can run `cargo clippy` locally to get the same effect. I've also
    taken the liberty to nail down the rustfmt version to use, so we can
    update it explicitly. (This is the same CI setup that assert_cli uses.)
    
    * Get rid of all dependencies for now
    
    Improves compile times :trollface:
    
    * Use termcolor for colored output
    
    This should make it compatible with windows consoles.
    
    * Set up some kind of error handling for the hook
    
    * rustfmt
    
    * Bump clippy
    
    and choose a nightly that actually exists.
    
    * Make clippy happy

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,55 @@
-#![feature(external_doc)]
-#![doc(include = "../README.md")]
-#![deny(missing_docs)]
-#![cfg_attr(test, deny(warnings))]
-#![cfg_attr(test, feature(plugin))]
-#![cfg_attr(test, plugin(clippy))]
+#![cfg_attr(feature = "nightly", deny(missing_docs))]
+#![cfg_attr(feature = "nightly", feature(external_doc))]
+#![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
 
-extern crate console;
-extern crate failure;
-extern crate unindent;
+extern crate termcolor;
 
-use console::style;
-use failure::Error;
+use std::error::Error;
 use std::panic;
-use unindent::unindent;
 
 /// Catch any error handlers that occur, and
 // Cargo env vars available:
 // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-crates
-pub fn catch_unwind<F: FnOnce() -> Result<(), Error>>(f: F) {
+pub fn catch_unwind<F: FnOnce() -> Result<(), Box<Error>>>(f: F) {
   panic::set_hook(Box::new(|_panic_info| {
     // TODO: create log report.
-    print_msg();
+    if let Err(e) = print_msg() {
+      eprintln!("Error generating panic message: {}", e);
+    }
   }));
 
-  match f() {
-    Ok(_) => {}
-    _ => { /* TODO: create log report. */ }
+  if let Err(_e) = f() {
+    // TODO: create log report.
   }
 }
 
-fn print_msg() {
+use std::io::{Result as IoResult, Write};
+use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
+
+fn print_msg() -> IoResult<()> {
+  let stderr = BufferWriter::stderr(ColorChoice::Auto);
+  let mut buffer = stderr.buffer();
+  buffer.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
+
   let _version = env!("CARGO_PKG_VERSION");
   let name = env!("CARGO_PKG_NAME");
   let authors = env!("CARGO_PKG_AUTHORS");
   let homepage = env!("CARGO_PKG_HOMEPAGE");
 
-  let mut msg = unindent(&format!(r#"
-      Well, this is embarrasing.
-
-      {} had a problem and crashed. To help us diagnose the problem you can send us a crash report.
-
-      We have generated a report file at "<reports not generated yet>". Submit an issue or email with the subject of "{} Crash Report" and include the report as an attachment.
-    "#, name, name));
-  msg.push_str("\n");
+  writeln!(&mut buffer, "Well, this is embarrasing.\n")?;
+  writeln!(&mut buffer, "{} had a problem and crashed. To help us diagnose the problem you can send us a crash report.\n", name)?;
+  writeln!(&mut buffer, "We have generated a report file at \"<reports not generated yet>\". Submit an issue or email with the subject of \"{} Crash Report\" and include the report as an attachment.\n", name)?;
 
   if !homepage.is_empty() {
-    msg.push_str(&format!("- Homepage: {}\n", homepage));
+    writeln!(&mut buffer, "- Homepage: {}", homepage)?;
   }
   if !authors.is_empty() {
-    msg.push_str(&format!("- Authors: {}\n", authors));
+    writeln!(&mut buffer, "- Authors: {}", authors)?;
   }
-  msg.push_str("\nWe take privacy seriously, and do not perform any automated error collection. In order to improve the software, we rely on people to submit reports.\n");
-  msg.push_str("\nThank you kindly!");
-  eprintln!("{}", style(msg).red());
+  writeln!(&mut buffer, "\nWe take privacy seriously, and do not perform any automated error collection. In order to improve the software, we rely on people to submit reports.\n")?;
+  writeln!(&mut buffer, "Thank you kindly!")?;
+
+  stderr.print(&buffer).unwrap();
+
+  Ok(())
 }
