f73007aa1e33a02d417aac97269d02062b30a5f8
Katharina Fey <kookie@spacekookie.de>
2018-05-28 17:13:42 +0200

    Adding documentation ✨

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,53 @@
+//! Panic handlers for human beings
+//!
+//! [![crates.io version][1]][2] [![build status][3]][4]
+//! [![downloads][5]][6] [![docs.rs docs][7]][8]
+//!
+//! Panic messages for humans. Replacement for
+//! [`std::panic::catch_unwind`](https://doc.rust-lang.org/std/panic/fn.catch_unwind.html)
+//! to make errors nice for humans.
+//!
+//! - [Documentation][8]
+//! - [Crates.io][2]
+//!
+//! ## Why?
+//! When you're building a CLI, polish is super important. Even though Rust is
+//! pretty great at safety, it's not unheard of to access the wrong index in a
+//! vector or have an assert fail somewhere.
+//!
+//! When an error eventually occurs, you probably will want to know about it. So
+//! instead of just providing an error message on the command line, we can create a
+//! call to action for people to submit a report.
+//!
+//! This should empower people to engage in communication, lowering the chances
+//! people might get frustrated. And making it easier to figure out what might be
+//! causing bugs.
+//!
+//! ### Default Output
+//!
+//! ```txt
+//! thread 'main' panicked at 'oops', examples/main.rs:2:3
+//! note: Run with `RUST_BACKTRACE=1` for a backtrace.
+//! ```
+//!
+//! ### Human-Panic Output
+//!
+//! ```txt
+//! Well, this is embarrassing.
+//!
+//! human-panic had a problem and crashed. To help us diagnose the problem you can send us a crash report.
+//!
+//! We have generated a report file at "/var/folders/zw/bpfvmq390lv2c6gn_6byyv0w0000gn/T/report-8351cad6-d2b5-4fe8-accd-1fcbf4538792.toml". Submit an issue or email with the subject of "human-panic Crash Report" and include the report as an attachment.
+//!
+//! - Homepage: https://github.com/yoshuawuyts/human-panic
+//! - Authors: Yoshua Wuyts <yoshuawuyts@gmail.com>
+//!
+//! We take privacy seriously, and do not perform any automated error collection. In order to improve the software, we rely on people to submit reports.
+//!
+//! Thank you kindly!
+
 #![cfg_attr(feature = "nightly", deny(missing_docs))]
 #![cfg_attr(feature = "nightly", feature(external_doc))]
-#![cfg_attr(feature = "nightly", doc(include = "../README.md"))]
 
 extern crate backtrace;
 #[macro_use]
@@ -30,8 +77,23 @@ pub struct Metadata {
   pub homepage: Cow<'static, str>,
 }
 
-/// Setup the human panic hook that will make all panics
-/// as beautiful as your shitty code.
+/// `human-panic` initialisation macro
+///
+/// You can either call this macro with no arguments `setup_panic!()` or
+/// with a Metadata struct, if you don't want the error message to display
+/// the values used in your `Cargo.toml` file.
+///
+/// The Metadata struct can't implement `Default` because of orphan rules, which
+/// means you need to provide all fields for initialisation.Metadata
+///
+/// ```rust norun
+/// setup_panic!(Metadata {
+///     name: env!("CARGO_PKG_NAME").into(),
+///     version: env!("CARGO_PKG_VERSION").into(),
+///     authors: "My Company Support <support@mycompany.com".into(),
+///     homepage: "support.mycompany.com".into(),
+/// });
+/// ```
 #[macro_export]
 macro_rules! setup_panic {
   ($meta:expr) => {
