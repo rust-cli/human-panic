c85564f46563ea63b51761e618004bed0eff901c
Ed Page <eopage@gmail.com>
2024-10-10 12:07:35 -0500

    test: Check report file

--- tests/single-panic/tests/integration.rs
+++ tests/single-panic/tests/integration.rs
@@ -1,7 +1,16 @@
 #[test]
 #[cfg_attr(debug_assertions, ignore)]
 fn release() {
+    let root = snapbox::dir::DirRoot::mutable_temp().unwrap();
+    let root_path = root.path().unwrap();
+
+    #[cfg(unix)]
+    let envs = [("TMPDIR", root_path)];
+    #[cfg(not(unix))]
+    let envs: [(&str, &str); 0] = [];
+
     snapbox::cmd::Command::new(snapbox::cmd::cargo_bin!("single-panic-test"))
+        .envs(envs)
         .assert()
         .stderr_eq(snapbox::str![[r#"
 Well, this is embarrassing.
@@ -18,12 +27,58 @@ Thank you kindly!
 
 "#]])
         .code(101);
+
+    #[cfg(unix)]
+    {
+        let mut files = root_path
+            .read_dir()
+            .unwrap()
+            .map(|e| {
+                let e = e.unwrap();
+                let path = e.path();
+                let content = std::fs::read_to_string(&path);
+                (path, content)
+            })
+            .collect::<Vec<_>>();
+        assert_eq!(files.len(), 1, "{files:?}");
+        let (_, report) = files.pop().unwrap();
+        let report = report.unwrap();
+        snapbox::assert_data_eq!(
+            report,
+            snapbox::str![[r#"
+"name" = "single-panic-test"
+"operating_system" = "[..]"
+"crate_version" = "0.1.0"
+"explanation" = """
+Panic occurred in file 'tests/single-panic/src/main.rs' at line [..]
+"""
+"cause" = "OMG EVERYTHING IS ON FIRE!!!"
+"method" = "Panic"
+"backtrace" = """
+
+...
+[..]"""
+
+"#]]
+        );
+    }
+
+    root.close().unwrap();
 }
 
 #[test]
 #[cfg_attr(not(debug_assertions), ignore)]
 fn debug() {
+    let root = snapbox::dir::DirRoot::mutable_temp().unwrap();
+    let root_path = root.path().unwrap();
+
+    #[cfg(unix)]
+    let envs = [("TMPDIR", root_path)];
+    #[cfg(not(unix))]
+    let envs: [(&str, &str); 0] = [];
+
     snapbox::cmd::Command::new(snapbox::cmd::cargo_bin!("single-panic-test"))
+        .envs(envs)
         .assert()
         .stderr_eq(snapbox::str![[r#"
 thread 'main' panicked at tests/single-panic/src/main.rs:7:5:
@@ -32,4 +87,21 @@ note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
 
 "#]])
         .code(101);
+
+    #[cfg(unix)]
+    {
+        let files = root_path
+            .read_dir()
+            .unwrap()
+            .map(|e| {
+                let e = e.unwrap();
+                let path = e.path();
+                let content = std::fs::read_to_string(&path);
+                (path, content)
+            })
+            .collect::<Vec<_>>();
+        assert!(files.is_empty(), "{files:?}");
+    }
+
+    root.close().unwrap();
 }
